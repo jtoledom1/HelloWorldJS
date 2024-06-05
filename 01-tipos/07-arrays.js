5a0b388ce92c140908bdb872042e07edf15b6d6b
Diego <100723562+jtoledom1@users.noreply.github.com>
2024-06-05 04:51:27 -0600

    🐛 Arrays

---
+++ 01-tipos/07-arrays.js
@@ -0,0 +1,7 @@
+let reptiles = []; //Array literal
+let animal =['lizard','Dragonutes'];//elementos del array empiezan desde el índice 0
+console.log(animal);
+console.log(animal[0]);
+animal[2]='gato';
+console.log(animal);
+console.log(typeof animal);
